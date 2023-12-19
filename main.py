fe44839d53cbb8cd3628a88c4ab8096d503c5552
jstobart01 <106041653+jstobart01@users.noreply.github.com>
2023-12-18 21:11:31 -0800

    Added styles, html, and python call

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-# This program will be a scheduler that uses the idea of Flex Planning to
-# help you plan your day based on how you feel (i.e. your energy and motivation levels)
-# Started 2023-12-26
+from flask import Flask, render_template
+from waitress import serve
+
+# Jackson's Calculator
+# Menu Page
+
+app = Flask(__name__)
+
+# Calling the main page
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+# For local hosting
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=8000)
